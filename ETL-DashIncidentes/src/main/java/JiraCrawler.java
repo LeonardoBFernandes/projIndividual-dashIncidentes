19debce4e6bfc8198d8400d12a6699db8df496cb
LeonardoBFernandes <leonardo.fernandes@sptech.school>
2025-11-16 14:33:05 -0300

    Adicionando função que busca os dados para a tabela da dash

--- ETL-DashIncidentes/src/main/java/JiraCrawler.java
+++ ETL-DashIncidentes/src/main/java/JiraCrawler.java
@@ -7,13 +7,18 @@ import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class JiraCrawler {
-    public static void main(String[] args) {
+    //Query usando affected hardware: "project = MONA AND \"Affected hardware\" ~ \"Nome do componente\""
+    public static void main(String[] args) throws JsonProcessingException {
         System.out.println(buscarDadosKpisIncidentes());
+        System.out.println(buscarDadosRankingComponentes());
     }
 
-    public static int fazerConsultaKpisIncidentes(String query) {
+    public static int fazerConsultaContador(String query) {
         JsonNodeFactory jnf = JsonNodeFactory.instance;
         ObjectNode payload = jnf.objectNode();
         {
@@ -57,11 +62,44 @@ public class JiraCrawler {
     public static ObjectNode buscarDadosKpisIncidentes() {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode dadosKpisIncidentes = mapper.createObjectNode();
-        dadosKpisIncidentes.put("totais", fazerConsultaKpisIncidentes("project = MONA"));
-        dadosKpisIncidentes.put("abertos", fazerConsultaKpisIncidentes("project = MONA AND status = open"));
-        dadosKpisIncidentes.put("fechados", fazerConsultaKpisIncidentes("project = MONA AND status in (completed, canceled, closed)"));
-        dadosKpisIncidentes.put("atencao", fazerConsultaKpisIncidentes("project = MONA AND summary ~ \"\\\"ATENÇÃO\\\"\""));
-        dadosKpisIncidentes.put("criticos", fazerConsultaKpisIncidentes("project = MONA AND summary ~ \"\\\"ALERTA CRÍTICO\\\"\""));
+        dadosKpisIncidentes.put("totais", fazerConsultaContador("project = MONA"));
+        dadosKpisIncidentes.put("abertos", fazerConsultaContador("project = MONA AND status in (open, 'in progress')"));
+        dadosKpisIncidentes.put("fechados", fazerConsultaContador("project = MONA AND status in (completed, canceled, closed)"));
+        dadosKpisIncidentes.put("atencao", fazerConsultaContador("project = MONA AND summary ~ 'ATENÇÃO'"));
+        dadosKpisIncidentes.put("criticos", fazerConsultaContador("project = MONA AND summary ~ 'ALERTA CRÍTICO'"));
         return dadosKpisIncidentes;
+    }
+
+    public static List buscarDadosRankingComponentes() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<ObjectNode> dadosRankingComponentes = new ArrayList<>();
+        String[] componentes = {"CPU", "Memória RAM", "Disco", "Latência", "Uso de Rede"};
+        for (String componente : componentes) {
+            ObjectNode dadosComponente = mapper.createObjectNode();
+            dadosComponente.put("nomeComponente", componente);
+            dadosComponente.put("totais", fazerConsultaContador("project = MONA AND \"Affected hardware\" ~ '"+componente+"'"));
+            dadosComponente.put("atencao", fazerConsultaContador("project = MONA AND summary ~ 'ATENÇÃO' AND \"Affected hardware\" ~ '"+componente+"'"));
+            dadosComponente.put("criticos", fazerConsultaContador("project = MONA AND summary ~ 'ALERTA CRÍTICO' AND \"Affected hardware\" ~ '"+componente+"'"));
+            dadosRankingComponentes.add(dadosComponente);
+        }
+
+        //Fazendo ordenação da lista de jsons
+        //Primeiro é necessário transformar a lista em String
+        String listaAsString = mapper.writeValueAsString(dadosRankingComponentes);
+        //Aqui está sendo feito o mapeamento do json para list
+        List<Map<String, Object>> listaFinalOrdenada = mapper.readValue(listaAsString, List.class);
+        //Aqui é feita a comparação dos valores
+        listaFinalOrdenada.sort((jsonA, jsonB) -> {
+            // Acessa o valor da chave de ordenação em cada Map (JSON)
+            // Assumimos que o valor é um Integer e fazemos o cast.
+            Integer valorA = (Integer) jsonA.get("totais");
+            Integer valorB = (Integer) jsonB.get("totais");
+
+            // Compara os valores (retorna a ordem crescente por padrão)
+            // Colocando primeiro valorB e depois valorA é retornado ordem decrescente
+            return valorB.compareTo(valorA);
+        });
+
+        return listaFinalOrdenada;
     }
 }
