9d554bb7a225f841c6f6b16cef2ba0886fd39e70
LeonardoBFernandes <leonardo.fernandes@sptech.school>
2025-11-20 19:58:37 -0300

    Adicionando função de calcular MTBF

--- ETL-DashIncidentes/src/main/java/JiraCrawler.java
+++ ETL-DashIncidentes/src/main/java/JiraCrawler.java
@@ -8,18 +8,21 @@ import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JiraCrawler {
     //Query usando affected hardware: "project = MONA AND \"Affected hardware\" ~ \"Nome do componente\""
     public static void main(String[] args) throws JsonProcessingException {
-        System.out.println(buscarDadosKpisIncidentes());
-        System.out.println(buscarDadosRankingComponentes());
-        buscarDadosIncidentes("project = MONA AND created <= 30d ORDER BY created ASC");
+        //System.out.println(buscarDadosKpisIncidentes());
+        //System.out.println(buscarDadosRankingComponentes());
+        System.out.println(calcularMTBF());
     }
 
-    public static void buscarDadosIncidentes(String query) {
+    public static ObjectNode buscarDadosIncidentes(String query) throws JsonProcessingException {
         JsonNodeFactory jnf = JsonNodeFactory.instance;
         ObjectNode payload = jnf.objectNode();
         {
@@ -59,6 +62,12 @@ public class JiraCrawler {
                 .asJson();
 
         System.out.println(response.getBody());
+
+        //Convertendo do tipo HttpResponse<JsonNode> para ObjectNode
+        ObjectNode json = new ObjectMapper()
+                .readValue(response.getBody().toString(), ObjectNode.class);
+
+        return json;
     }
 
     public static int fazerConsultaContador(String query) {
@@ -115,12 +124,11 @@ public class JiraCrawler {
         return listaParaEnvio;
     }
 
-    public static List<ObjectNode> buscarDadosRankingComponentes() throws JsonProcessingException {
+    public static List<ObjectNode> buscarDadosComponentes() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         List<ObjectNode> dadosRankingComponentes = new ArrayList<>();
         String[] componentes = {"CPU", "Memória RAM", "Disco", "Latência", "Uso de Rede"};
 
-        // Monta a lista de ObjectNode
         for (String componente : componentes) {
             ObjectNode dadosComponente = mapper.createObjectNode();
             dadosComponente.put("nomeComponente", componente);
@@ -130,14 +138,15 @@ public class JiraCrawler {
             dadosRankingComponentes.add(dadosComponente);
         }
 
-        // Ordena diretamento a lista de ObjectNode
+        //Aqui é feita a comparação dos valores
         dadosRankingComponentes.sort((jsonA, jsonB) -> {
             int valorA = jsonA.get("totais").asInt();
             int valorB = jsonB.get("totais").asInt();
-            return Integer.compare(valorB, valorA); // ordem decrescente
+            // Compara os valores (retorna a ordem crescente por padrão)
+            // Colocando primeiro valorB e depois valorA é retornado ordem decrescente
+            return Integer.compare(valorB, valorA);
         });
 
-        // Agora encontra o(s) componente(s) com mais incidentes
         List<String> componentesComMaisIncidentes = new ArrayList<>();
         int maiorNumeroIncidentesTotais = 0;
 
@@ -154,11 +163,51 @@ public class JiraCrawler {
             }
         }
 
-        // Adiciona o item final à lista
         ObjectNode resumo = mapper.createObjectNode();
         resumo.put("componentesComMaisIncidentes", String.join(", ", componentesComMaisIncidentes));
         dadosRankingComponentes.add(resumo);
 
         return dadosRankingComponentes;
+    }
+
+    public static ObjectNode calcularMTBF() {
+        ObjectMapper mapper = new ObjectMapper();
+        List<ZonedDateTime> datas = new ArrayList<>();
+        ObjectNode incidentesArray = null;
+        try {
+            incidentesArray = buscarDadosIncidentes("project = MONA AND created <= 30d ORDER BY created ASC");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        com.fasterxml.jackson.databind.JsonNode issuesArray = incidentesArray.path("issues");
+
+        for (com.fasterxml.jackson.databind.JsonNode issue : issuesArray) {
+            com.fasterxml.jackson.databind.JsonNode data = issue.path("fields").path("created");
+
+            String dataAsText = data.asText();
+            ZonedDateTime dt = ZonedDateTime.parse(dataAsText, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
+            datas.add(dt);
+        }
+
+        List<Duration> intervalos = new ArrayList<>();
+
+        for (int i = datas.size() - 1; i > 0; i--) {
+            Duration dur = Duration.between(datas.get(i - 1), datas.get(i));
+            intervalos.add(dur);
+        }
+
+        double mtbfMinutos = intervalos.stream()
+                .mapToDouble(Duration::toMinutes)
+                .average()
+                .orElse(0);
+
+        long horas = (long) (mtbfMinutos / 60);
+        long minutos = (long) (mtbfMinutos % 60);
+
+        ObjectNode json = mapper.createObjectNode();
+        json.put("MTBF", horas + "h" + minutos + "min");
+
+        return json;
     }
 }
